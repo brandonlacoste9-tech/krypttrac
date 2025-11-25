ff4c770206b925bc6b31bb26b674d51811d23c2e
Brandon Leroux <brandonlacoste9-tech@users.noreply.github.com>
2025-11-25 07:00:14 -0500

    feat: add Next.js and Tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          900: '#0f172a',
+        },
+        purple: {
+          400: '#c084fc',
+          500: '#a855f7',
+          600: '#9333ea',
+          700: '#7e22ce',
+          900: '#581c87',
+        },
+        pink: {
+          400: '#f472b6',
+          500: '#ec4899',
+          600: '#db2777',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
